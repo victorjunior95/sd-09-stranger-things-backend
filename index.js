38c7b1ddc61bc6002ce7d9179da9b879b01600f9
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-08-14 18:50:15 -0300

    Heroku deploy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -6,6 +7,9 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
+
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+const hereIsTheUpsideDown = /^true$/.test(UPSIDEDOWN_MODE);
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -16,8 +20,6 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
-
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
@@ -27,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
