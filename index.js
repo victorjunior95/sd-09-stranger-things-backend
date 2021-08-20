33557dd0c4d3e388368ce1d728dd698bc2a892e5
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-08-20 00:30:12 -0300

    fix error index line 11

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const PORT  = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
